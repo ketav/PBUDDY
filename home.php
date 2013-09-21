95cae4cd97690f9e79838564a8407347df5d96fc
ketav <ketavsharma@gmail.com>
2013-09-21 17:32:22 +0530

    Synched Data for fb sdk

--- home.php
+++ home.php
@@ -1,9 +1,50 @@
+<?php
+  // Remember to copy files from the SDK's src/ directory to a
+  // directory in your application on the server, such as php-sdk/
+  require_once('php-sdk/src/facebook.php');
+
+  $config = array(
+    'appId' => '188647124563048',
+    'secret' => 'dd9e0b9c9ee31990f0f12744fe88892d',
+  );
+
+  $facebook = new Facebook($config);
+  $user_id = $facebook->getUser();
+?>
 <html>
  <head>
   <title>PHP Test</title>
  </head>
  <body>
+ <?php
+    if($user_id) {
 
+      // We have a user ID, so probably a logged in user.
+      // If not, we'll get an exception, which we handle below.
+      try {
+
+        $user_profile = $facebook->api('/me','GET');
+        echo "Name: " . $user_profile['name'];
+
+      } catch(FacebookApiException $e) {
+        // If the user is logged out, you can have a 
+        // user ID even though the access token is invalid.
+        // In this case, we'll get an exception, so we'll
+        // just ask the user to login again here.
+        $login_url = $facebook->getLoginUrl(); 
+        echo 'Please <a href="' . $login_url . '">login.</a>';
+        error_log($e->getType());
+        error_log($e->getMessage());
+      }   
+    } else {
+
+      // No user, print a link for the user to login
+      $login_url = $facebook->getLoginUrl();
+      echo 'Please <a href="' . $login_url . '">login.</a>';
+
+    }
+
+  ?>
 <div class="fb-login-button" data-width="200"></div>
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
@@ -12,8 +53,7 @@ if (d.getElementById(id)) return;
 js = d.createElement(s); js.id = id;
 js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=188647124563048";
 fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
- <?php echo '<p>Hello World</p>'; ?> 
+}(document, 'script', 'facebook-jssdk'));</script> 
  </body>
 </html>
 
