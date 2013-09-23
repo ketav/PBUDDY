42b23c5f5d8f1559ea79951ade1aa6d8d9a2a747
Arash021987 <asingh58@sapient.com>
2013-09-23 21:18:16 +0530

    Adding stored procs

--- Resources/tables/StoredProcedures/pb_InsertPhoto.sql
+++ Resources/tables/StoredProcedures/pb_InsertPhoto.sql
@@ -1,9 +1,5 @@
-Delimiter //
-Create Procedure pb_InsertPhoto(
-IN id INT(100), IN src VARCHAR(1000), IN pid INT(100)
-)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `pb_InsertPhoto`(IN `id` INT(100), IN `src` VARCHAR(1000), IN `pid` INT(100))
 BEGIN
     INSERT INTO photo_detail (user_id, photo_url, avg_rating,rating_received,photo_id,active_flag)
 	VALUES (id, src,NULL,NULL,pid,1);
-END //
-Delimiter;
+END
