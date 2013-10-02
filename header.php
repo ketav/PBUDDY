9ef29ccee8078c0da7a6f41fdd6ec1b2159eb7cb
ketav <ketavsharma@gmail.com>
2013-10-02 10:46:53 +0530

    header minor changes plus album page css changes

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <div id="header">
 		<div class="headerData">
-					<a title="Miko" href="/default.aspx">
+					<a title="Miko" href="/pbuddy/index.php">
 					<figure class="logoFig"><img title="Miko" alt="Miko" src="/pbuddy/Resources/Images/logo.png"></figure>
 					</a>					
 					<nav id="nav" class="nav">
 						<ul>
 							<li class="seprator" style="width:158.6px;"><a href="../">Login</a></li>
-							<li style="width:158.6px;"><a href="../profile.php">Profile</a></li>
+							<li style="width:158.6px;"><a href="/pbuddy/profile.php">Profile</a></li>
                         </ul>
 					</div>	
 		</div>
